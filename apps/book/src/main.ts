74903daa32ab9837e071b1904ad3412ebdecac5c
CK Ng <ngchoonkhon@gmail.com>
2023-11-18 23:32:09 +0800

    revert changes

--- apps/book/src/main.ts
+++ apps/book/src/main.ts
@@ -15,8 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  console.log('test');
-
   await app.listen(3000);
 }
 bootstrap();
