b4aa2d1298d2592d9635c9a70d474af3f4268fa2
CK Ng <ngchoonkhon@gmail.com>
2023-11-18 23:30:26 +0800

    test ignore

--- apps/customer/src/main.ts
+++ apps/customer/src/main.ts
@@ -12,6 +12,8 @@ async function bootstrap() {
     .addTag('Customer')
     .build();
 
+  console.log('test');
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
