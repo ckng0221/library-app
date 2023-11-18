749bbaef79531340973f9f699aef4d3a28879997
CK Ng <ngchoonkhon@gmail.com>
2023-11-18 23:31:05 +0800

    test effect

--- apps/book/src/main.ts
+++ apps/book/src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  console.log('test');
+
   await app.listen(3000);
 }
 bootstrap();
