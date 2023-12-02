c2b920ada9b25b6af9475480e1fd2c73e70215b1
CK Ng <ngchoonkhon@gmail.com>
2023-12-02 18:19:41 +0800

    test: add payment e2e

--- apps/payment/test/app.e2e-spec.ts
+++ apps/payment/test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { INestApplication } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { mock } from 'jest-mock-extended';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, Model, Types, connect } from 'mongoose';
 import * as request from 'supertest';
-import { PaymentModule } from './../src/payment.module';
+import { PaymentController } from '../src/payment.controller';
+import { PaymentService } from '../src/payment.service';
+import { PaymentDtoStub } from '../src/dto/payment.dto.stub';
+import { Payment, PaymentSchema } from '../src/schemas/payment.schema';
 
-describe('PaymentController (e2e)', () => {
+describe('Payment (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [PaymentModule],
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let paymentModel: Model<Payment>;
+  let testPayment: any;
+
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    paymentModel = mongoConnection.model(Payment.name, PaymentSchema);
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [PaymentController],
+      providers: [
+        PaymentService,
+        { provide: getModelToken(Payment.name), useValue: paymentModel },
+        { provide: 'PAYMENT', useValue: mock<AmqpConnection>() },
+      ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  beforeEach(async () => {
+    testPayment = await new paymentModel(
+      PaymentDtoStub({
+        borrowing_id: new Types.ObjectId(),
+      }),
+    ).save();
+  });
+
+  afterEach(async () => {
+    const collections = mongoConnection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  });
+
+  it('/payments (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/payments')
+      .send({
+        borrowing_id: new Types.ObjectId(),
+      })
+      .expect(201);
+  });
+
+  it('/payments/{id} (GET)', () => {
+    return request(app.getHttpServer())
+      .get(`/payments/${testPayment._id}`)
+      .expect(200);
+  });
+
+  it('/payments/{id} (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete(`/payments/${testPayment._id}`)
+      .expect(200);
   });
 });
