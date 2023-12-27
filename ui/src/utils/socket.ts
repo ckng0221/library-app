50eca3fc4583d6d3d3bafb6a54c907c123820c1e
CK Ng <ngchoonkhon@gmail.com>
2023-12-27 19:31:08 +0800

    fix: fix sview issue
    Fix path and socket io issue

--- ui/src/utils/socket.ts
+++ ui/src/utils/socket.ts
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
 
-const PAYMENT_URL =
-  process.env.NODE_ENV === 'production'
-    ? undefined
-    : import.meta.env.VITE_PAYMENT_API_BASE_URL;
+const PAYMENT_URL = import.meta.env.VITE_PAYMENT_API_BASE_URL;
 
 export const paymentSocket = io(PAYMENT_URL);
