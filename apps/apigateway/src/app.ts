bf6cb580212c6f93be096b8dbe9db6600eb8cf74
CK Ng <ngchoonkhon@gmail.com>
2024-01-05 00:17:37 +0800

    feat: add http proxy

--- apps/apigateway/src/app.ts
+++ apps/apigateway/src/app.ts
@@ -1,9 +1,13 @@
+import 'dotenv/config';
 import * as express from 'express';
 import { setupLogging } from './logging';
+import { setupProxies } from './proxy';
+import { ROUTES } from './routes';
 
 const app = express();
 
 setupLogging(app);
+setupProxies(app, ROUTES);
 
 app.get('/', (req, res) => {
   return res.send('Welcome to API Gateway!');
--- apps/apigateway/src/routes.ts
+++ apps/apigateway/src/routes.ts
@@ -1,32 +1,101 @@
-const ROUTES = [
+import 'dotenv/config';
+import { Options } from 'http-proxy-middleware';
+
+export interface IRoute {
+  url: string;
+  auth?: boolean;
+  creditCheck?: boolean;
+  rateLimit?: { windowMs: number; max: number };
+  proxy: Options;
+}
+
+const BASEURL_BOOK = process.env.BASEURL_BOOK;
+const BASEURL_CUSTOMER = process.env.BASEURL_CUSTOMER;
+const BASEURL_BORROWING = process.env.BASEURL_BORROWING;
+const BASEURL_PAYMENT = process.env.BASEURL_PAYMENT;
+
+export const ROUTES: IRoute[] = [
+  // Book
   {
-    url: '/free',
+    url: '/api/book',
     auth: false,
     creditCheck: false,
-    rateLimit: {
-      windowMs: 15 * 60 * 1000,
-      max: 5,
-    },
     proxy: {
-      target: 'https://www.google.com',
+      target: BASEURL_BOOK,
       changeOrigin: true,
       pathRewrite: {
-        [`^/free`]: '',
+        '^/api/book': '',
       },
     },
   },
+  // Customer
   {
-    url: '/premium',
-    auth: true,
-    creditCheck: true,
+    url: '/api/customer',
+    auth: false,
+    creditCheck: false,
     proxy: {
-      target: 'https://www.google.com',
+      target: BASEURL_CUSTOMER,
       changeOrigin: true,
       pathRewrite: {
-        [`^/premium`]: '',
+        '^/api/customer': '',
+      },
+    },
+  },
+  // Borrowing
+  {
+    url: '/api/borrowing',
+    auth: false,
+    creditCheck: false,
+    proxy: {
+      target: BASEURL_BORROWING,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/borrowing': '',
+      },
+    },
+  },
+  // Customer
+  {
+    url: '/api/payment',
+    auth: false,
+    creditCheck: false,
+    proxy: {
+      target: BASEURL_PAYMENT,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/payment': '',
       },
     },
   },
 ];
 
-exports.ROUTES = ROUTES;
+// export const ROUTES: IRoute[] = [
+//   {
+//     url: '/free',
+//     auth: false,
+//     creditCheck: false,
+//     rateLimit: {
+//       windowMs: 15 * 60 * 1000,
+//       max: 5,
+//     },
+//     proxy: {
+//       target: 'https://www.google.com',
+//       changeOrigin: true,
+//       pathRewrite: {
+//         [`^/free`]: '',
+//       },
+//     },
+//   },
+//   {
+//     url: '/premium',
+//     auth: true,
+//     creditCheck: true,
+//     proxy: {
+//       target: 'https://www.google.com',
+//       changeOrigin: true,
+//       pathRewrite: {
+//         [`^/premium`]: '',
+//       },
+//     },
+//   },
+// ];
